f02a78606aff75fed894e129fbdc39add84fd898
0red0 <yhyawy1@gmail.com>
2022-05-29 19:30:31 +0200

    Fourth commit, testing the new git terminal.

--- script.js
+++ script.js
@@ -2,4 +2,19 @@ let arr = ["rock", "paper", "scissors"];
 
 let computerPlay = () => arr[Math.floor(Math.random() * arr.length)];
 
-console.log(computerPlay());
+// console.log(computerPlay());
+
+function playRound(playerSelection, computerSelection) {
+  return `The winner of the round`;
+}
+const playerSelection = "rock";
+const computerSelection = computerPlay();
+return playRound(playerSelection, computerSelection);
+
+function game(playRound) {
+  for (let i = 0; i < 5; i++) {
+      switch(true){
+          case 
+      }
+  }
+}
